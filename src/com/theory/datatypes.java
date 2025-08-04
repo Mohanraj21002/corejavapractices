679f72182b7368676a3dd4cb36fa89a65ee331c1
Mohanraj <mohanmoaj970@gamil.com>
2025-08-04 15:40:01 +0530

    "day 1 datatypes.java"

--- src/com/theory/datatypes.java
+++ src/com/theory/datatypes.java
@@ -2,6 +2,31 @@ package com.theory;
 
 public class datatypes {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        // Integer types
+        byte age = 25;
+        short year = 2025;
+        int salary = 50000;
+        long distance = 9876543210L;
+
+        // Floating point types
+        float pi = 3.14f;
+        double gravity = 9.80665;
+
+        // Character
+        char grade = 'A';
+
+        // Boolean
+        boolean isJavaFun = true;
+
+        // Output
+        System.out.println("Age (byte): " + age);
+        System.out.println("Year (short): " + year);
+        System.out.println("Salary (int): " + salary);
+        System.out.println("Distance (long): " + distance);
+        System.out.println("PI (float): " + pi);
+        System.out.println("Gravity (double): " + gravity);
+        System.out.println("Grade (char): " + grade);
+        System.out.println("Is Java fun? (boolean): " + isJavaFun);
+
     }
 }
