39ad228db763d2e819944a3724d4a3be7e5b72dc
Mohanraj <mohanmoaj970@gamil.com>
2025-09-09 15:25:27 +0530

    "Added DatatypesExamples.java"

---
+++ src/com/datatypes/DatatypesExamples.java
@@ -0,0 +1,18 @@
+package com.datatypes;
+public class DatatypesExamples {
+        public static void main(String[] args) {
+            int age = 25;
+            double salary = 50000.75;
+            char grade = 'A';
+            boolean isStudent = false;
+            String name = "Venkatesh";
+
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Salary: " + salary);
+            System.out.println("Grade: " + grade);
+            System.out.println("Is Student: " + isStudent);
+        }
+    }
+
+
